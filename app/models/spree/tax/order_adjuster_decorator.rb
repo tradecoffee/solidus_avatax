86f08da6fb5e9ad790d7b40d0237891be990fb56
Joe Lind <joelind@gmail.com>
2017-09-01 07:51:27 -0400

    Update module declaration to prevent load order dependency issue

--- app/models/spree/tax/order_adjuster_decorator.rb
+++ app/models/spree/tax/order_adjuster_decorator.rb
@@ -1,8 +1,12 @@
-module Solidus::Avatax::OrderAdjuster
-  def adjust!
-    # do nothing. we hook in in our own ways.
-    # TODO: See if we can make OrderAdjuster pluggable and workable for what we
-    # need to do.
+module Solidus
+  module Avatax
+    module OrderAdjuster
+      def adjust!
+        # do nothing. we hook in in our own ways.
+        # TODO: See if we can make OrderAdjuster pluggable and workable for what we
+        # need to do.
+      end
+    end
   end
 end
 
